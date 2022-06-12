ea977cc79e876ab5ffbba801e1c1af2ef9d55e52
Craig Neuwirt <cneuwirt@gmail.com>
2022-06-12 18:20:01 -0500

    Add ouput validation in filter

--- validates.go
+++ validates.go
@@ -313,27 +313,101 @@ func (v validateFilter) Next(
 	ctx      HandleContext,
 	provider FilterProvider,
 )  (out []any, pout *promise.Promise[[]any], err error) {
-	if _, ok := provider.(*ValidateProvider); ok {
+	if vp, ok := provider.(*ValidateProvider); ok {
 		callback := ctx.Callback()
 		composer := ctx.Composer()
 		outcomeIn, poi, errIn := Validate(composer, callback.Source())
 		if errIn != nil {
+			// error validating input
 			return nil, nil, errIn
 		}
 		if poi == nil {
+			// if invalid return input results
 			if !outcomeIn.Valid() {
 				return nil, nil, outcomeIn
 			}
-			return next.Pipe()
-		}
-		return nil, promise.Then(poi, func(outIn *ValidationOutcome) []any {
-			if !outIn.Valid() {
-				panic(outIn)
+			// perform the next step in the pipeline
+			if out, pout, err = next.Pipe(); !(err == nil && vp.validateResult) {
+				// if error or skip output validation, return output
+				return
+			} else if pout == nil {
+				// validate output if available
+				if len(out) > 0 && !IsNil(out[0]) {
+					outcomeOut, poo, errOut := Validate(composer, out[0])
+					if errOut != nil {
+						// error validating so return
+						return nil, nil, errOut
+					}
+					if poo == nil {
+						// synchronous output validation
+						if !outcomeOut.Valid() {
+							// invalid so return output results
+							return nil, nil, outcomeOut
+						}
+					} else {
+						// asynchronous output validation
+						return nil, promise.Then(poo, func(outcome *ValidationOutcome) []any {
+							// if invalid return output results
+							if !outcome.Valid() {
+								panic(outcome)
+							}
+							return out
+						}), nil
+					}
+				}
+				return
+			} else {
+				// asynchronous output validation
+				return nil, promise.Then(pout, func(oo []any) []any {
+					if len(oo) > 0 && !IsNil(oo[0]) {
+						outcomeOut, poo, errOut := Validate(composer, oo[0])
+						if errOut != nil {
+							// error validating input
+							panic(errOut)
+						}
+						if poo != nil {
+							// resolve output validation results
+							if outcomeOut, errOut = poo.Await(); errOut != nil {
+								// resolution failed so return
+								panic(errOut)
+							}
+						} else if !outcomeOut.Valid() {
+							// invalid so return output results
+							panic(outcomeOut)
+						}
+					}
+					return oo
+				}), nil
 			}
-			return next.PipeAwait()
+		}
+		// asynchronous input validation
+		return nil, promise.Then(poi, func(outcome *ValidationOutcome) []any {
+			// if invalid return input results
+			if !outcome.Valid() {
+				panic(outcome)
+			}
+			oo := next.PipeAwait()
+			// validate output if requested and available
+			if vp.validateResult && len(oo) > 0 && !IsNil(oo[0]) {
+				outcomeOut, poo, errOut := Validate(composer, oo[0])
+				if errOut != nil {
+					// error validating output
+					panic(errOut)
+				}
+				if poo != nil {
+					// resolve output validation results
+					if outcomeOut, errOut = poo.Await(); errOut != nil {
+						// resolution failed so return
+						panic(errOut)
+					}
+				} else if !outcomeOut.Valid() {
+					// invalid so return output results
+					panic(outcomeOut)
+				}
+			}
+			return oo
 		}), nil
 	}
-	// todo: validate result
 	return next.Abort()
 }
 
