244d3c68443c0a757c932b4e51fe0a300261f19f
Craig Neuwirt <cneuwirt@gmail.com>
2023-02-05 13:00:04 -0600

    Generated openapi components

--- api/http/httpsrv/openapi/feature.go
+++ api/http/httpsrv/openapi/feature.go
@@ -1,14 +1,86 @@
 package openapi
 
 import (
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/getkin/kin-openapi/openapi3gen"
 	"github.com/miruken-go/miruken"
+	"github.com/miruken-go/miruken/api"
 	"github.com/miruken-go/miruken/api/http/httpsrv"
 	"github.com/miruken-go/miruken/handles"
+	"github.com/miruken-go/miruken/validates"
+	"reflect"
+	"time"
 )
 
 // Installer configures openapi support
 type Installer struct {
-	handlesPolicy miruken.Policy
+	policy         miruken.Policy
+	schemas        openapi3.Schemas
+	requestBodies  openapi3.RequestBodies
+	responses      openapi3.Responses
+	paths          openapi3.Paths
+	generator      *openapi3gen.Generator
+	requests       map[reflect.Type]struct{}
+	typeInfoFormat string
+}
+
+func (i *Installer) Describe(api *openapi3.T)  {
+	if api == nil {
+		panic("api cannot be nil")
+	}
+	components := api.Components
+	if components == nil {
+		components = new(openapi3.Components)
+		api.Components = components
+	}
+	if schemas := i.schemas; len(schemas) > 0 {
+		if components.Schemas == nil {
+			components.Schemas = make(openapi3.Schemas)
+		}
+		for name, schema := range i.schemas {
+			if _, ok := components.Schemas[name]; ok {
+				break
+			} else {
+				components.Schemas[name] = schema
+			}
+		}
+	}
+	if requestBodies := i.requestBodies; len(requestBodies) > 0 {
+		if components.RequestBodies == nil {
+			components.RequestBodies = make(openapi3.RequestBodies)
+		}
+		for name, reqBody := range i.requestBodies {
+			if _, ok := components.RequestBodies[name]; ok {
+				break
+			} else {
+				components.RequestBodies[name] = reqBody
+			}
+		}
+	}
+	if responses := i.responses; len(responses) > 0 {
+		if components.Responses == nil {
+			components.Responses = make(openapi3.Responses)
+		}
+		for name, response := range i.responses {
+			if _, ok := components.Responses[name]; ok {
+				break
+			} else {
+				components.Responses[name] = response
+			}
+		}
+	}
+	if paths := i.paths; len(paths) > 0 {
+		if api.Paths == nil {
+			api.Paths = make(openapi3.Paths)
+		}
+		for name, path := range i.paths {
+			if _, ok := api.Paths[name]; ok {
+				break
+			} else {
+				api.Paths[name] = path
+			}
+		}
+	}
 }
 
 func (i *Installer) DependsOn() []miruken.Feature {
@@ -18,9 +90,47 @@ func (i *Installer) DependsOn() []miruken.Feature {
 func (i *Installer) Install(setup *miruken.SetupBuilder) error {
 	if setup.Tag(&featureTag) {
 		var h handles.It
-		i.handlesPolicy = h.Policy()
+		i.policy        = h.Policy()
+		i.schemas = make(openapi3.Schemas)
+		i.requestBodies = make(openapi3.RequestBodies)
+		i.responses     = make(openapi3.Responses)
+		i.paths         = make(openapi3.Paths)
+		i.requests      = make(map[reflect.Type]struct{})
+		i.generator     = openapi3gen.NewGenerator(
+			openapi3gen.UseAllExportedFields(),
+			openapi3gen.SchemaCustomizer(i.customize))
 		setup.Observers(i)
 	}
+	return nil
+}
+
+func (i *Installer) AfterInstall(
+	*miruken.SetupBuilder, miruken.Handler,
+) error {
+	for _, extra := range extraTypes {
+		_, _ = i.generator.GenerateSchemaRef(extra)
+	}
+	Loop:
+	for typ, schema := range i.generator.Types {
+		if typ.Kind() == reflect.Ptr {
+			typ = typ.Elem()
+		}
+		if typ.Kind() == reflect.Struct {
+			for _, ex := range skipTypes {
+				if ex == typ {
+					continue Loop
+				}
+			}
+			i.schemas[schema.Ref] = schema
+			schema.Ref = ""
+		}
+	}
+	i.responses["NoResponse"] = &openapi3.ResponseRef{
+		Value: openapi3.NewResponse().
+			WithContent(openapi3.NewContentWithJSONSchema(openapi3.NewSchema())),
+	}
+	i.generator = nil
+	i.requests  = nil
 	return nil
 }
 
@@ -29,23 +139,71 @@ func (i *Installer) BindingCreated(
 	descriptor *miruken.HandlerDescriptor,
 	binding    miruken.Binding,
 ) {
-	if !(policy == i.handlesPolicy  && binding.Exported()) {
+	if !(policy == i.policy && binding.Exported()) {
 		return
 	}
-	/*
 	if inputType, ok := binding.Key().(reflect.Type); ok {
 		if inputType.Kind() == reflect.Ptr {
 			inputType = inputType.Elem()
 		}
-		path := strings.Replace(inputType.String(), ".", "/", 1)
-		ep := endpoint.New("post", "/process/" + path, "",
-			endpoint.BodyType(i.schema(inputType), "request to process", true),
-			endpoint.ResponseType(http.StatusOK, i.schema(binding.LogicalOutputType()), "Successfully handled"),
-			endpoint.Response(http.StatusInternalServerError, api.ErrorData{}, "Oops ... something went wrong"),
-			endpoint.Tags(inputType.PkgPath()),
-		)
+		if inputType.Kind() != reflect.Struct {
+			return
+		}
+		if _, ok := i.requests[inputType]; ok {
+			return
+		}
+		if _, err := i.generator.GenerateSchemaRef(inputType); err != nil {
+			return
+		} else {
+			reqSchema   := i.generator.Types[inputType]
+			requestBody := &openapi3.RequestBodyRef{
+				Value: openapi3.NewRequestBody().
+					WithDescription("Request to process.").
+					WithRequired(true).
+					WithJSONSchemaRef(openapi3.NewSchemaRef(
+						"#/schemas/schemas/" + reqSchema.Ref, nil)),
+				}
+			requestName := reqSchema.Ref + "Request"
+			i.requestBodies[requestName] = requestBody
+
+			responseName := "NoResponse"
+			if outputType := binding.LogicalOutputType(); outputType != nil {
+				if outputType.Kind() == reflect.Ptr {
+					outputType = outputType.Elem()
+				}
+				if outputType.Kind() == reflect.Struct && outputType.NumField() > 0 {
+					if _, err := i.generator.GenerateSchemaRef(outputType); err != nil {
+						return
+					} else {
+						respSchema := i.generator.Types[outputType]
+						response := &openapi3.ResponseRef{
+							Value: openapi3.NewResponse().
+								WithDescription("Successfully handled.").
+								WithContent(openapi3.NewContentWithJSONSchemaRef(
+									openapi3.NewSchemaRef("#/schemas/schemas/"+respSchema.Ref, nil))),
+						}
+						responseName = respSchema.Ref + "Response"
+						i.responses[responseName] = response
+					}
+				}
+			}
+			path := &openapi3.PathItem{
+				Post: &openapi3.Operation{
+					OperationID: reqSchema.Ref,
+					RequestBody: &openapi3.RequestBodyRef{
+						Ref: "#/schemas/requestBodies/" + requestName,
+					},
+					Responses: openapi3.Responses{
+						"200": &openapi3.ResponseRef{
+							Ref: "#/schemas/responses/" + responseName,
+						},
+					},
+					Tags: []string{inputType.PkgPath()},
+				},
+			}
+			i.paths["/process/" + reqSchema.Ref] = path
+		}
 	}
-	 */
 }
 
 func (i *Installer) DescriptorCreated(
@@ -53,6 +211,30 @@ func (i *Installer) DescriptorCreated(
 ) {
 }
 
+func (i *Installer) customize(
+	name   string,
+	t      reflect.Type,
+	tag    reflect.StructTag,
+	schema *openapi3.Schema,
+) error {
+	if props := schema.Properties; props != nil {
+		for i, prop := range props {
+			if prop.Value.Type == "object" {
+				props[i] = &openapi3.SchemaRef{
+					Ref: "#/schemas/schemas/" + prop.Ref,
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// TypeInfoFormat selects how the type discriminator is generated.
+func TypeInfoFormat(format string ) func(installer *Installer) {
+	return func(installer *Installer) {
+		installer.typeInfoFormat = format
+	}
+}
 
 // Feature configures http server support
 func Feature(config ...func(installer *Installer)) *Installer {
@@ -65,9 +247,11 @@ func Feature(config ...func(installer *Installer)) *Installer {
 	return installer
 }
 
-
 var (
 	featureTag byte
-	anyType     = miruken.TypeOf[any]()
-	emptySchema = miruken.TypeOf[struct{}]()
+	skipTypes  = []reflect.Type{miruken.TypeOf[time.Time]()}
+	extraTypes = []reflect.Type{
+		miruken.TypeOf[validates.ApiOutcome](),
+		miruken.TypeOf[api.ErrorData](),
+	}
 )
--- api/http/httpsrv/openapi/test/openapi_test.go
+++ api/http/httpsrv/openapi/test/openapi_test.go
@@ -1,10 +1,8 @@
 package test
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/getkin/kin-openapi/openapi3"
-	"github.com/getkin/kin-openapi/openapi3gen"
 	"github.com/miruken-go/miruken"
 	"github.com/miruken-go/miruken/api"
 	"github.com/miruken-go/miruken/api/http"
@@ -16,7 +14,6 @@ import (
 	"github.com/miruken-go/miruken/promise"
 	"github.com/stretchr/testify/suite"
 	"net/http/httptest"
-	"reflect"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -25,30 +22,79 @@ import (
 //go:generate $GOPATH/bin/miruken -tests
 
 type (
+	Address struct {
+		Line  string
+		City  string
+		State string
+		Zip   string
+	}
+
+	PlayerData struct {
+		Id        int32
+		Name      string
+		BirthDate time.Time
+		Address   Address
+		Version   int32
+	}
+
 	CreatePlayer struct {
 		Name      string
 		BirthDate time.Time
+		Address   Address
+	}
+
+	UpdatePlayer struct {
+		Id        int32
+		Name      string
+		BirthDate time.Time
+		Address   Address
 	}
 
 	PlayerResult struct {
-		PlayerId int32
+		Id      int32
+		Version int32
 	}
 
 	PlayerHandler struct {
 		nextId int32
+		store  map[int32]*PlayerData
 	}
 )
 
+func (p *PlayerHandler) Constructor() {
+	p.store = make(map[int32]*PlayerData)
+}
+
 func (p *PlayerHandler) CreatePlayer(
-	_ *handles.It, create *CreatePlayer,
-	ctx miruken.HandleContext,
+	_ *handles.It, create CreatePlayer,
 ) *promise.Promise[PlayerResult] {
-	id := atomic.AddInt32(&p.nextId,1)
-	return promise.Resolve(PlayerResult{id})
+	id     := atomic.AddInt32(&p.nextId,1)
+	player := PlayerData{
+		Id:        id,
+		Name:      create.Name,
+		BirthDate: create.BirthDate,
+		Address:   create.Address,
+		Version:   1,
+	}
+	p.store[id] = &player
+	return promise.Resolve(PlayerResult{id, player.Version})
+}
+
+func (p *PlayerHandler) UpdatePlayer(
+	_ *handles.It, update UpdatePlayer,
+) *promise.Promise[PlayerResult] {
+	if player, ok := p.store[update.Id]; !ok {
+		nf := fmt.Errorf("player with id %v not found", update.Id)
+		return promise.Reject[PlayerResult](nf)
+	} else {
+		player.Version++
+		return promise.Resolve(PlayerResult{player.Id, player.Version})
+	}
 }
 
 type OpenApiTestSuite struct {
 	suite.Suite
+	openapi *openapi.Installer
 	srv *httptest.Server
 }
 
@@ -62,8 +108,9 @@ func (suite *OpenApiTestSuite) Setup(specs ...any) *context.Context {
 }
 
 func (suite *OpenApiTestSuite) SetupTest() {
+	suite.openapi = openapi.Feature()
 	handler, _ := miruken.Setup(
-		TestFeature, openapi.Feature(), jsonstd.Feature()).
+		TestFeature, jsonstd.Feature(), suite.openapi).
 		Specs(&api.GoTypeFieldInfoMapper{}).
 		Handler()
 	suite.srv = httptest.NewServer(httpsrv.Api(context.New(handler)))
@@ -76,16 +123,28 @@ func (suite *OpenApiTestSuite) TearDownTest() {
 
 func (suite *OpenApiTestSuite) TestOpenApi() {
 	suite.Run("Generates OpenApi", func() {
-		schemas := make(openapi3.Schemas)
-		customizer := openapi3gen.SchemaCustomizer(func(name string, ft reflect.Type, tag reflect.StructTag, schema *openapi3.Schema) error {
-			return nil
-		})
-		schemaRef, err := openapi3gen.NewSchemaRefForValue(&CreatePlayer{}, schemas,
-			openapi3gen.UseAllExportedFields(), openapi3gen.ThrowErrorOnCycle(), customizer)
-		suite.Nil(err)
-		suite.NotNil(schemaRef)
-		data, err := json.MarshalIndent(schemaRef, "", "  ")
-		fmt.Printf("%s\n", data)
+		api := openapi3.T{
+			OpenAPI: "3.0.0",
+			Info: &openapi3.Info{
+				Title:       "Team Api",
+				Description: "REST Api used for managing Teams",
+				Version:     "0.0.0",
+				License: &openapi3.License{
+					Name: "MIT",
+					URL:  "https://opensource.org/licenses/MIT",
+				},
+				Contact: &openapi3.Contact{
+					URL: "https://github.com/craig/team-microservice",
+				},
+			},
+			Servers: openapi3.Servers{
+				&openapi3.Server{
+					Description: "Local development",
+					URL:         "http://127.0.0.1:9234",
+				},
+			},
+		}
+		suite.openapi.Describe(&api)
 	})
 }
 
