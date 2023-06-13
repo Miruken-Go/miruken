642e55e92b55fba468cfd3fd789f9eb25a6e91ff
Michael  Dudley <provenstyle@gmail.com>
2023-06-13 17:18:39 -0500

    prepending the v to the version number

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 docker run -v $(pwd):/go/src --workdir=/go/src golang:1.20 go test ./...
+#Todo: how do I fail the script if the tests fail????
 
-TAG=$(docker run --rm -v "$(pwd):/repo" gittools/gitversion:5.12.0-alpine.3.14-6.0 /repo /showvariable SemVer)
+TAG="v$(docker run --rm -v "$(pwd):/repo" gittools/gitversion:5.12.0-alpine.3.14-6.0 /repo /showvariable SemVer)"
 echo "Build Version: $TAG"
 
 git tag $TAG
