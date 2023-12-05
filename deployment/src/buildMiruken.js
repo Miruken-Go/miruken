19007f0b13d9e4a3f344639768400da7d85db585
Michael  Dudley <mirukenjs@gmail.com>
2023-12-04 19:58:22 -0600

    updating to the latest ci.cd version

--- deployment/src/buildMiruken.js
+++ deployment/src/buildMiruken.js
@@ -2,6 +2,7 @@ import * as bash     from '#infrastructure/bash.js'
 import * as logging  from '#infrastructure/logging.js'
 import * as git      from '#infrastructure/git.js'
 import * as gh       from '#infrastructure/gh.js'
+import { handle }    from '#infrastructure/handler.js'
 import { variables } from '#infrastructure/envVariables.js'
 
 variables.requireEnvVariables([
@@ -12,42 +13,30 @@ variables.optionalEnvVariables([
     'skipRepositoryDispatches'
 ])
 
-async function main() {
-    try {
-        logging.printEnvironmentVariables(variables)
+handle(async () => {
+    logging.printEnvironmentVariables(variables)
 
-        logging.header("Building miruken")
+    logging.header("Building miruken")
 
-        await bash.execute(`
-            go test ./...
-        `)
+    await bash.execute(`
+        go test ./...
+    `)
 
-        //This docker container is running docker in docker from github actions
-        //Therefore using $(pwd) to get the working directory would be the working directory of the running container 
-        //Not the working directory from the host system. So we need to pass in the repository path.
-        const rawVersion = await bash.execute(`
-            docker run --rm -v '${variables.repositoryPath}:/repo' \
-            gittools/gitversion:5.12.0-alpine.3.14-6.0 /repo /showvariable SemVer
-        `)
+    //This docker container is running docker in docker from github actions
+    //Therefore using $(pwd) to get the working directory would be the working directory of the running container 
+    //Not the working directory from the host system. So we need to pass in the repository path.
+    const rawVersion = await bash.execute(`
+        docker run --rm -v '${variables.repositoryPath}:/repo' \
+        gittools/gitversion:5.12.0-alpine.3.14-6.0 /repo /showvariable SemVer
+    `)
 
-        const gitTag = `v${rawVersion}`
+    const gitTag = `v${rawVersion}`
 
-        console.log(`gitTag: [${gitTag}]`)
+    console.log(`gitTag: [${gitTag}]`)
 
-        await git.tagAndPush(gitTag)
+    await git.tagAndPush(gitTag)
 
-        if (!variables.skipRepositoryDispatches) {
-            await gh.sendRepositoryDispatches('Miruken-Go', 'miruken-version-created', {
-                version: gitTag
-            })
-        }
-
-        console.log("Script completed successfully")
-    } catch (error) {
-        process.exitCode = 1
-        console.log(error)
-        console.log("Script Failed")
-    }
-}
-
-main()
+    await gh.sendRepositoryDispatches('built-miruken', {
+        mirukenVersion: gitTag
+    })
+})
