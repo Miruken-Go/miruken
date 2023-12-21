6db97b33d82531def6a545bae6d0de289b16e14d
Craig Neuwirt <cneuwirt@gmail.com>
2023-12-21 08:38:27 -0600

    Fixed FeatureSet to handle dependencies

--- setup/feature.go
+++ setup/feature.go
@@ -1,6 +1,8 @@
 package setup
 
-import "github.com/miruken-go/miruken/internal"
+import (
+	"github.com/miruken-go/miruken/internal"
+)
 
 type (
 	// Feature encapsulates custom setup.
@@ -8,22 +10,48 @@ type (
 		Install(*Builder) error
 	}
 	FeatureFunc func(*Builder) error
+
+	featureSet struct {
+		features []Feature
+	}
 )
 
 func (f FeatureFunc) Install(b *Builder) error {
 	return f(b)
 }
 
-// FeatureSet combines one or more Feature's into a single logical Feature.
-func FeatureSet(features ...Feature) FeatureFunc {
-	return func(b *Builder) error {
-		for _, feature := range features {
+
+// featureSet
+
+func (f *featureSet) DependsOn() []Feature {
+	var deps []Feature
+	for _, feature := range f.features {
+		if !internal.IsNil(feature) {
+			if dependsOn, ok := feature.(interface {
+				DependsOn() []Feature
+			}); ok {
+				deps = append(deps, dependsOn.DependsOn()...)
+			}
+		}
+	}
+	return deps
+}
+
+func (f *featureSet) Install(b *Builder) error {
+	if b.Tag(f) {
+		for _, feature := range f.features {
 			if !internal.IsNil(feature) {
 				if err := feature.Install(b); err != nil {
 					return err
 				}
 			}
 		}
-		return nil
 	}
+	return nil
+}
+
+
+// FeatureSet combines one or more Feature's into a single logical Feature.
+func FeatureSet(features ...Feature) Feature {
+	return &featureSet{features: features}
 }
